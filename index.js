2486d9fa58b4e48f99233e0776258b70f7666557
Kiran Wankhade <kiranwankhade7738@gmail.com>
2024-09-17 15:01:05 +0530

    data added

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const sequelize = require('./lib/sequelize');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+// Sync the database
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connected and synchronized.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 app.get('/', (req, res) => {
-  return res.send('Starter template');
+  console.log('hello');
+  return res.json({ message: 'Starter template' });
 });
 
 app.listen(PORT, () => console.log('Server is running on port', PORT));
